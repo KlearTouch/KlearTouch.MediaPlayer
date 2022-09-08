7f0d502f3536d17df8a73c9182aaf8f11242aee6
PierreHenriKT <PierreHenriKT+Git@KlearTouch.com>
2022-09-08 12:14:47 +0100

    Sample using WinUI 3 MediaPlayerElement (with the Windows App SDK version 1.2 Preview 1)

--- samples/KlearTouchApp.MediaPlayer.WinUI.Xaml/MainWindow.xaml.cs
+++ samples/KlearTouchApp.MediaPlayer.WinUI.Xaml/MainWindow.xaml.cs
@@ -2,23 +2,18 @@
 
 using System;
 using Microsoft.UI.Xaml;
-using MediaSource = Microsoft.UI.Media.Core.MediaSource;
+using FastPlayFallbackBehaviour = Microsoft.UI.Xaml.Media.FastPlayFallbackBehaviour;
+using MediaSource = Windows.Media.Core.MediaSource;
 
 namespace KlearTouchApp.MediaPlayer.WinUI
 {
-    public class MediaPlayerElement : Microsoft.UI.Xaml.Controls.MediaPlayerElement { }
-
     public sealed partial class MainWindow
     {
-        public Microsoft.UI.Xaml.Controls.MediaPlayerElement MediaPlayerElement { get; } = new() { AreTransportControlsEnabled = true };
-
         public MainWindow()
         {
             InitializeComponent();
 
-            Title = "KlearTouch MediaPlayer for WinUI 3 (XAML)";
-
-            MediaPlayerElementContainer!.Child = MediaPlayerElement;
+            Title = "Sample using WinUI 3 MediaPlayerElement (XAML)";
         }
 
         private async void OpenFile(object sender, RoutedEventArgs e)
@@ -36,26 +31,46 @@ namespace KlearTouchApp.MediaPlayer.WinUI
             var file = await picker.PickSingleFileAsync()!;
             if (file is null) return;
 
-            if (MediaPlayerElement.MediaPlayer is null) MediaPlayerElement.SetMediaPlayer(new());
+            if (MediaPlayerElement!.MediaPlayer is null) MediaPlayerElement.SetMediaPlayer(new());
+
+            var item = new Windows.Media.Playback.MediaPlaybackItem(MediaSource.CreateFromStorageFile(file)!);
+            var props = item.GetDisplayProperties()!;
+            props.Type = Windows.Media.MediaPlaybackType.Video;
+            props.VideoProperties!.Title = file.DisplayName;
+            props.VideoProperties.Subtitle = System.IO.Path.GetDirectoryName(file.Path);
+            item.ApplyDisplayProperties(props);
 
             MediaPlayerElement.MediaPlayer!.AutoPlay = true;
-            MediaPlayerElement.MediaPlayer.Source = MediaSource.CreateFromStorageFile(file);
+            MediaPlayerElement.MediaPlayer.Source = item;
         }
 
         private void OpenUri(object sender, RoutedEventArgs e)
         {
-            if (MediaPlayerElement.MediaPlayer is null) MediaPlayerElement.SetMediaPlayer(new());
+            if (MediaPlayerElement!.MediaPlayer is null) MediaPlayerElement.SetMediaPlayer(new());
 
             MediaPlayerElement.MediaPlayer!.AutoPlay = true;
             MediaPlayerElement.MediaPlayer.Source = MediaSource.CreateFromUri(new("https://sec.ch9.ms/ch9/8791/ab3e65c2-619c-4d36-b7ca-abd0499a8791/WinUI.mp4", UriKind.Absolute));
         }
 
-        private void Play(object sender, RoutedEventArgs e) => MediaPlayerElement.MediaPlayer?.Play();
+        private void Play(object sender, RoutedEventArgs e) => MediaPlayerElement!.MediaPlayer?.Play();
 
-        private void Pause(object sender, RoutedEventArgs e) => MediaPlayerElement.MediaPlayer?.Pause();
+        private void Pause(object sender, RoutedEventArgs e) => MediaPlayerElement!.MediaPlayer?.Pause();
 
-        private void CleanUpMediaPlayer(object sender, RoutedEventArgs e) => MediaPlayerElement.SetMediaPlayer(null!);
+        private void CleanUpMediaPlayer(object sender, RoutedEventArgs e) => MediaPlayerElement!.SetMediaPlayer(null!);
 
-        private void ToggleTransportControls(object sender, RoutedEventArgs e) => MediaPlayerElement.AreTransportControlsEnabled = !MediaPlayerElement.AreTransportControlsEnabled;
+        private void ToggleButtonsVisibility(object sender, RoutedEventArgs e)
+        {
+            var mtc = MediaPlayerElement!.TransportControls!;
+            //mtc.IsFullWindowButtonVisible = mtc.IsFullWindowEnabled = mtc.IsCompactOverlayButtonVisible = mtc.IsCompactOverlayEnabled =
+            mtc.IsZoomButtonVisible = mtc.IsZoomEnabled = mtc.IsFastForwardButtonVisible = mtc.IsFastForwardEnabled = mtc.IsFastRewindButtonVisible = mtc.IsFastRewindEnabled = mtc.IsStopButtonVisible = mtc.IsStopEnabled =
+                mtc.IsVolumeButtonVisible = mtc.IsVolumeEnabled = mtc.IsPlaybackRateButtonVisible = mtc.IsPlaybackRateEnabled = mtc.IsSeekBarVisible = mtc.IsSeekEnabled =
+                    mtc.IsSkipForwardButtonVisible = mtc.IsSkipForwardEnabled = mtc.IsSkipBackwardButtonVisible = mtc.IsSkipBackwardEnabled = mtc.IsNextTrackButtonVisible = mtc.IsPreviousTrackButtonVisible =
+                        mtc.IsRepeatEnabled = mtc.IsRepeatButtonVisible = !mtc.IsRepeatButtonVisible;
+            mtc.FastPlayFallbackBehaviour = FastPlayFallbackBehaviour.Disable;
+        }
+
+        private void ToggleAutoHide(object sender, RoutedEventArgs e) => MediaPlayerElement!.TransportControls!.ShowAndHideAutomatically = !MediaPlayerElement.TransportControls.ShowAndHideAutomatically;
+
+        private void ToggleCompact(object sender, RoutedEventArgs e) => MediaPlayerElement!.TransportControls!.IsCompact = !MediaPlayerElement.TransportControls.IsCompact;
     }
 }
